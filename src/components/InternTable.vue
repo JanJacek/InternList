c1e3ab77072c3f68c16105a2b83370e3fe5aac03
JanJacek <bobkiewicz.jacek@gmail.com>
2024-10-14 13:14:09 +0200

    Adjust table for editing a user

--- src/components/InternTable.vue
+++ src/components/InternTable.vue
@@ -1,29 +1,27 @@
 <template>
-  <div style="width: 100%">
+  <div style="width: 100%; min-width: 300px">
     <p class="text-h5 q-ml-xl text-grey-8">User list</p>
-    <div
-      class="q-ml-xl q-mr-xl bg-grey-1 q-pa-md"
-      v-if="u_store.isDataComplete"
-    >
+    <div class="q-ml-xl q-mr-xl bg-grey-1 q-pa-md">
       <q-table
         :flat="true"
         :rows="filteredRows"
         :columns="columns"
         row-key="name"
-        style="width: 100%; height: 360px"
+        style="width: 100%; height: 400px"
         class="bg-grey-1 no-border-rows"
         separator="none"
         hide-pagination
         v-model:pagination="pagination"
       >
         <template v-slot:top>
-          <div class="q-my-md" style="width: 100%">
+          <div class="q-my-md flex justify-between" style="width: 100%">
             <q-input
               v-model="u_store.search"
               debounce="300"
+              class="q-mr-md q-mb-md"
               outlined
-              placeholder="Search"
-              style="width: 100%; max-width: 400px"
+              placeholder="Search for users..."
+              style="width: 80%; max-width: 400px"
               dense
               filled
             >
@@ -31,7 +29,17 @@
                 <q-icon name="search" />
               </template>
             </q-input>
+            <q-btn
+              label="Add User"
+              no-caps
+              class="q-mb-md"
+              icon="add"
+              color="primary"
+              rounded
+              @click="goToAddUser"
+            />
           </div>
+          <div class="bg-grey-3" style="width: 100%; height: 1px"></div>
         </template>
 
         <template v-slot:body="props">
@@ -44,6 +52,29 @@
                 <q-avatar size="lg">
                   <img :src="props.row.avatar" />
                 </q-avatar>
+              </template>
+              <template v-else-if="col.name === 'actions'">
+                <div class="q-gutter-xs">
+                  <q-btn
+                    @click="goToEditUser(props.row.id)"
+                    flat
+                    size="xs"
+                    class="q-pa-none"
+                  >
+                    <img
+                      src="../assets/edit-icon.svg"
+                      alt="Edit"
+                      class="q-icon"
+                    />
+                  </q-btn>
+                  <q-btn
+                    icon="delete"
+                    color="grey-5"
+                    flat
+                    size="xs"
+                    class="q-pa-none"
+                  />
+                </div>
               </template>
               <template v-else>
                 {{ props.row[col.field] }}
@@ -59,9 +90,9 @@
         v-model="pagination.page"
         direction-links
         outline
-        color="teal-9"
+        color="primary"
         active-design="unelevated"
-        active-color="teal-9"
+        active-color="primary"
         active-text-color="white"
         :rounded="false"
         gutter="3px"
@@ -74,7 +105,9 @@
 <script setup lang="ts">
 import { uiStore } from 'src/stores/uiStore';
 import { computed, ref } from 'vue';
+import { useRouter } from 'vue-router';
 const u_store = uiStore();
+const router = useRouter();
 
 const columns: {
   name: string;
@@ -88,7 +121,7 @@ const columns: {
     name: 'avatar',
     label: '',
     field: 'avatar',
-    sortable: true,
+    sortable: false,
     align: 'left',
     style: 'width: 50px',
   },
@@ -103,11 +136,11 @@ const columns: {
     name: 'actions',
     label: 'Action',
     field: 'id',
-    sortable: true,
-    align: 'right',
+    sortable: false,
+    align: 'left',
+    style: 'width: 140px',
   },
 ];
-
 const filteredRows = computed(() => u_store.filteredTableRowData);
 
 const pagination = ref({
@@ -115,15 +148,25 @@ const pagination = ref({
   rowsPerPage: 5,
 });
 
-const pagesNumber = Math.ceil(
-  u_store.users.length / pagination.value.rowsPerPage
+const pagesNumber = computed(() =>
+  Math.ceil(Object.keys(u_store.users).length / pagination.value.rowsPerPage)
 );
 
-// const rows = u_store.tableRowData;
+const goToAddUser = () => {
+  console.log('hello');
+  router.push({ path: '/user/new' });
+};
+
+const goToEditUser = (id: number) => {
+  router.push({ path: `/user/${id}` });
+};
 </script>
 
 <style>
 .q-table__top {
   padding: 0px !important;
+}
+.q-icon {
+  fill: var(--q-color-grey-5);
 }
 </style>
